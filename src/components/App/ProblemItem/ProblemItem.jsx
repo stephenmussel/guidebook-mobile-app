2913fbbef5970d0eab6cfa6b1fdbc1839dc4c09c
phaydara <phaydara@gmail.com>
2022-06-14 16:11:56 -0500

    added V grade with cases for VB and Projects

--- src/components/App/ProblemItem/ProblemItem.jsx
+++ src/components/App/ProblemItem/ProblemItem.jsx
@@ -3,8 +3,12 @@ import './ProblemItem.css';
 function ProblemItem({ each }) {
     return (
         <div>
-            <p><b>{each.boulder_name}</b></p>
-            <p>{each.problem_num} {each.problem}</p>
+
+            {/* conditionally renders new boulder name if the problem_num is 1 (start of a new boulder) */}
+            <h3 className={each.problem_num != 1 ? '' : 'boulder-name'}>
+                <b>{each.problem_num === 1 ? each.boulder_name : ''}</b>
+            </h3><br />
+            {/* <p><b>{each.boulder_name}</b></p> */}
 
             {/* conditionally renders color of circle based on grade */}
             <p className={
@@ -18,6 +22,11 @@ function ProblemItem({ each }) {
                 {/* determines whether it's a problem or a variation. if it's a variation, the number is replaced with a `V` */}
                 {each.variation ? <b>V</b> : <b>{each.problem_num}</b>}
             </p>
+
+            {/* displays name of problem and grade like: V5, VB, Project, etc. */}
+            <p><b>{each.problem}</b> {each.grade === 'B' ? 'VB' : each.project ? 'Project' : 'V' + each.grade}</p>
+            {/* <p>{each.problem_num} {each.problem}</p> */}
+
             <p>stars: {each.stars}</p>
             <p>{each.description}</p>
             <p>fa: {each.fa}</p>
