cd68c06ba855c844c9e16995dba1c8c8b74e4629
phaydara <phaydara@gmail.com>
2022-06-14 11:50:21 -0500

    cleaned up files and added request to fetch problems

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const creatureRouter = require('./routes/creature.router.js');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -9,7 +8,6 @@ app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/creature', creatureRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
