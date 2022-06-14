a1d130f8b7a27be8621ad17a690d77c7503fb979
phaydara <phaydara@gmail.com>
2022-06-14 16:27:24 -0500

    displayed stars and added comments

--- src/components/App/ProblemItem/ProblemItem.jsx
+++ src/components/App/ProblemItem/ProblemItem.jsx
@@ -12,24 +12,35 @@ function ProblemItem({ each }) {
 
             {/* conditionally renders color of circle based on grade */}
             <p className={
-                each.grade <= 2 ? 'circle-green' :
-                    each.grade <= 5 ? 'circle-blue' :
-                        each.grade <= 8 ? 'circle-orange' :
-                            each.grade < 17 ? 'circle-pink' :
+                each.grade <= 2 ? 'circle-green' : // 0-2
+                    each.grade <= 5 ? 'circle-blue' : // 3-5
+                        each.grade <= 8 ? 'circle-orange' : // 6-8
+                            each.grade < 17 ? 'circle-pink' : // 9-16
                                 each.grade === 'B' ? 'circle-green' : // VB
-                                    each.project ? 'circle-black' : ''} // Project
+                                    each.project ? 'circle-black' : '' // Project
+            }
             >
                 {/* determines whether it's a problem or a variation. if it's a variation, the number is replaced with a `V` */}
                 {each.variation ? <b>V</b> : <b>{each.problem_num}</b>}
             </p>
+            <p>
+                {/* displays name of problem and grade like: V5, VB, Project, etc. */}
+                <b>{each.problem}</b> {each.grade === 'B' ? 'VB' : each.project ? 'Project' : 'V' + each.grade} {
 
-            {/* displays name of problem and grade like: V5, VB, Project, etc. */}
-            <p><b>{each.problem}</b> {each.grade === 'B' ? 'VB' : each.project ? 'Project' : 'V' + each.grade}</p>
+                    // TODO: there's a better way to do this!
+                    // displays number of stars for each problem
+                    each.stars === 5 ? '\u2605\u2605\u2605\u2605\u2605' :
+                        each.stars === 4 ? '\u2605\u2605\u2605\u2605' :
+                            each.stars === 3 ? '\u2605\u2605\u2605' :
+                                each.stars === 2 ? '\u2605\u2605' :
+                                    each.stars === 1 ? '\u2605' : ""
+                }
+            </p>
             {/* <p>{each.problem_num} {each.problem}</p> */}
 
-            <p>stars: {each.stars}</p>
+            {/* <p>stars: {each.stars}</p> */}
             <p>{each.description}</p>
-            <p>fa: {each.fa}</p>
+            {/* <p>fa: {each.fa}</p> */}
         </div>
     )
 }
